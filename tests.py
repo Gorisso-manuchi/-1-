12073c614e9629c409a8e05418b4bfcc54321465
Gorisso-manuchi <88540461+Gorisso-manuchi@users.noreply.github.com>
2022-09-03 17:44:14 +0300

    1 Глава
    Модуль string

---
+++ tests.py
@@ -0,0 +1,50 @@
+
+#Функция capwords () переводит в верхний регистр первую букву каждого слова в строке.
+
+import string
+s = 'The quick brown fox jumped over the lazy dog.'
+
+
+print(s)
+print(string.capwords(s))
+
+#The Quick Brown Fox Jumped Over The Lazy Dog.
+
+
+import string 
+   
+values = {'name':'Гор', 'firstname':'Манучарян', 'phd': 'Высшее'}
+t = string.Template(""" 
+
+Имя: $name
+Фамилие : $firstname
+Оброзование: ${phd} Образованние
+
+
+"""
+)
+print(t.substitute(values))
+
+
+#s = """
+#Имя: %(name)
+#Фамилие : %(firstname)
+#Оброзование: %(phd) Образованние
+#
+#"""
+#
+#print(s % values)
+
+s ="""
+Имя: {name}
+Фамилие : {firstname}
+Оброзование: {phd} Образованние
+
+
+"""
+print(s.format(**values))
+
+
+
+    
+ 
